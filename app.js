97b26a1d9c040074fca7959af017f745b1ec6e2e
Jing-Shan Wong <joshdragonwong@gmail.com>
2019-12-10 10:24:15 -0700

    hmtl plus more cards

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let x = 1
         }
         
         if (x % 2 == 1){
-            document.getElementById("card").src="https://www.maa.org/sites/default/files/images/columns/colm/card40.gif"
+            document.getElementById("card").src="https://www.publicdomainpictures.net/pictures/130000/nahled/check-mark-icon.jpg"
         }
         x++;
     } 
@@ -23,11 +23,11 @@ let x = 1
     card1.onclick = function() {
 
         if(y % 2 == 0){
-            document.getElementById("card1").src="https://i.pinimg.com/originals/85/1e/bd/851ebd52c5bafae169c4b983a42a0624.png"
+            document.getElementById("card1").src="https://previews.123rf.com/images/gearstd/gearstd1610/gearstd161000079/64199579-businessman-acting-like-he-is-punching-someone-on-a-white-background-facing-challenges-tough-competi.jpg"
         }
         
         if (y % 2 == 1){
-            document.getElementById("card1").src="https://www.maa.org/sites/default/files/images/columns/colm/card40.gif"
+            document.getElementById("card1").src="https://www.pnglot.com/pngfile/detail/23-233895_image-red-cross-wrong-cross-png-transparent.png"
         }
         y++;
     } 
